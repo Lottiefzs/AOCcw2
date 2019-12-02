025ff0e9721bb76a8d1a72cac34809cba58d1ed9
charl <fzs18vfu@uea.ac.uk>
2019-12-02 11:55:14 +0000

    Initialise function added, errors for includes fixed

--- part1.c
+++ part1.c
@@ -4,24 +4,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <vss.h>
+#include <stdbool.h>
 #include "part1.h"
 
+Node* first;
 
-typedef struct node{
+typedef struct Node{
     struct node * next;
     struct node * previous;
     bool isFree;
     size_t size;
-    void * memoryAddress;
-}node;
+}Node;
+
 
 void initialise ( void * memory , size_t size ){
-
+    first = (Node*) memory;
+    first->size = size - sizeof(Node);
+    first->isFree = true;
+    first->next = NULL;
+    first->previous = NULL;
 };
 
-void * allocate ( size_t bytes ){
-
+void* allocate ( size_t bytes ){
+    for (Node* node = first; node != NULL ; first = first->next) {
+        if(node->isFree == true && node->size >= (bytes + sizeof(Node))){
+            node->isFree = false;
+            Node* new = (Node*) node + (bytes + sizeof(Node));
+            new->next=node->next;
+            node->next = new;
+            new->previous = node;
+            new->isFree = true;
+            new->size = (node->size - sizeof(Node) - bytes);
+            node->size = bytes;
+        }
+    }
+    return NULL;
 };
 
 void deallocate ( void * memory ){
@@ -29,5 +46,8 @@ void deallocate ( void * memory ){
 };
 
 int main(){
+    void* heap = malloc(sizeof(200));
+    size_t size = 200;
+    initialise(&heap, size);
 
 }
--- part1.h
+++ part1.h
@@ -5,4 +5,12 @@
 #ifndef AOCCW2_PART1_H
 #define AOCCW2_PART1_H
 
+typedef struct Node Node;
+
+void initialise ( void *memory , size_t size );
+
+void *allocate ( size_t bytes );
+
+void deallocate ( void *memory );
+
 #endif //AOCCW2_PART1_H
